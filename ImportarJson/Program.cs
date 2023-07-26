1248d2987c88de8f8240698dc91da1db22d206f8
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-26 13:30:58 +0200

    Menu y pedirvalores para crear moneda agregados

--- ImportarJson/Program.cs
+++ ImportarJson/Program.cs
@@ -6,22 +6,54 @@ namespace ImportarJson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            string opcion = string.Empty;
 
-            string rutaJson = "..\\..\\..\\monedas.json";
-
-            // Guarda el contenido de monedas.json en la variable json
-            string json = File.ReadAllText(rutaJson);
-
-            // Deserializa el json en la lista de monedas
-            List<Monedas> listaMonedas = JsonConvert.DeserializeObject<List<Monedas>>(json);
-
-            foreach (Monedas moneda in listaMonedas) 
+            while (opcion != "3")
             {
-                string name = moneda.nombre;
-                string code = moneda.codigo;
-                float value = moneda.valorEnDolares;
-                Console.WriteLine($" Moneda: {name} | Codigo: {code} | Valor en USD: {value}.");
+                Console.Clear();
+                Console.WriteLine($"\n\t MONEDAS.JSON\n");
+                Console.WriteLine($"\t 1-. Listar monedas");
+                Console.WriteLine($"\t 2-. Crear monedas");
+                Console.WriteLine($"\t 3-. Salir");
+
+                Console.Write($"\n\n\t Seleccione una opción: ");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        // Ruta del fichero .json
+                        string rutaJson = "..\\..\\..\\monedas.json";
+
+                        // Guarda el contenido de monedas.json en la variable json
+                        string json = File.ReadAllText(rutaJson);
+
+                        // Deserializa el json en la lista de monedas
+                        List<Monedas> listaMonedas = JsonConvert.DeserializeObject<List<Monedas>>(json);
+
+                        foreach (Monedas moneda in listaMonedas)
+                        {
+                            string name = moneda.nombre;
+                            string code = moneda.codigo;
+                            float value = moneda.valorEnDolares;
+                            Console.WriteLine($" Moneda: {name} | Codigo: {code} | Valor en USD: {value}.");
+                        }
+                            break;
+                    case "2":
+                        // Pedimos valores
+                        Console.Write($" Nombre de la nueva moneda: ");
+                        string nombreNuevaMoneda = Console.ReadLine();
+                        Console.Write($" Codigo de la nueva moneda: ");
+                        string codigoNuevaMoneda = Console.ReadLine();
+                        Console.Write($" Valor de la nueva moneda: ");
+                        float valorEnDolares = float.Parse(Console.ReadLine());
+
+
+                        break;
+                    case "3":
+                        break;
+                }
+                
             }
         }
     }
