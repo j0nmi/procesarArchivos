e5819c287958163b33e7b63e538368558b8d9a4f
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-26 12:44:06 +0200

    Deserializa objeto JSON en una instancia de la clase Movie

---
+++ ImportarJson/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace ImportarJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"{
+              'Name': 'Bad Boys',
+              'ReleaseDate': '1995-4-7T00:00:00',
+              'Genres': [
+                'Action',
+                'Comedy'
+              ]
+            }";
+
+
+            // Deserializa y guarda el objeto de tipo Movie en la variable m
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+
+            // Se extrae el nombre de la pelicula y se guarda en la variable name
+            string name = m.Name;
+
+            Console.WriteLine(name);
+            // Bad Boys
+        }
+    }
+}
