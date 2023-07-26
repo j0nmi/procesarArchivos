fbeeb9e6aee18b3fb62bc1d561bf32bf90a9e287
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-26 13:11:16 +0200

    Corregido codigo y comentarios

--- ImportarJson/Program.cs
+++ ImportarJson/Program.cs
@@ -8,9 +8,12 @@ namespace ImportarJson
         {
             Console.WriteLine("");
 
-
             string rutaJson = "..\\..\\..\\monedas.json";
+
+            // Guarda el contenido de monedas.json en la variable json
             string json = File.ReadAllText(rutaJson);
+
+            // Deserializa el json en la lista de monedas
             List<Monedas> listaMonedas = JsonConvert.DeserializeObject<List<Monedas>>(json);
 
             foreach (Monedas moneda in listaMonedas) 
@@ -18,7 +21,7 @@ namespace ImportarJson
                 string name = moneda.nombre;
                 string code = moneda.codigo;
                 float value = moneda.valorEnDolares;
-                Console.WriteLine($" Moneda: {name} + Codigo: {code} + Valor: {value}.");
+                Console.WriteLine($" Moneda: {name} | Codigo: {code} | Valor en USD: {value}.");
             }
         }
     }
