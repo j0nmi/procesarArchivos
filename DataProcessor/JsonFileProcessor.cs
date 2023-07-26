29125ba1800fec31b134eb9520c0254066b03130
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-26 12:36:48 +0200

    Editado el Json Proccessor y agregado Movie.cs

--- DataProcessor/JsonFileProcessor.cs
+++ DataProcessor/JsonFileProcessor.cs
@@ -15,15 +15,19 @@ namespace DataProcessor
 
         public void Process()
         {
+            // Lee el fichero y lo almacena en la variable originalText
+
+            string originalText = File.ReadAllText(InputFilePath);
+
             // Using read all text
             //string originalText = File.ReadAllText(InputFilePath);
             //string processedText = originalText.ToUpperInvariant();
             //File.WriteAllText(OutputFilePath, processedText);
 
             // Using read all lines
-            string[] lines = File.ReadAllLines(InputFilePath);
-            lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file
-            File.WriteAllLines(OutputFilePath, lines);
+            //string[] lines = File.ReadAllLines(InputFilePath);
+            //lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file
+            //File.WriteAllLines(OutputFilePath, lines);
         }
     }
 }
---
+++ DataProcessor/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    // 1-. Copiamos el JSON
+    // 2-. Editar > Pegado especial > Pegar JSON como clases
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string ReleaseDate { get; set; }
+        public string[] Genres { get; set; }
+    }
+}
