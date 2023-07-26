4d80d91c73df63a879928e930c4a1e49125c577f
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-26 13:47:54 +0200

    Update creados metodos

--- ImportarJson/Program.cs
+++ ImportarJson/Program.cs
@@ -24,37 +24,54 @@ namespace ImportarJson
                     case "1":
                         Console.Clear();
                         Console.WriteLine($"\n\t LISTADO MONEDAS (monedas.json)\n");
-                        // Ruta del fichero .json
-                        string rutaJson = "..\\..\\..\\monedas.json";
-
-                        // Guarda el contenido de monedas.json en la variable json
-                        string json = File.ReadAllText(rutaJson);
-
-                        // Deserializa el json en la lista de monedas
-                        List<Monedas> listaMonedas = JsonConvert.DeserializeObject<List<Monedas>>(json);
-
-                        foreach (Monedas moneda in listaMonedas)
-                        {
-                            string name = moneda.nombre;
-                            string code = moneda.codigo;
-                            float value = moneda.valorEnDolares;
-                            Console.WriteLine($"\t Moneda: {name} | Codigo: {code} | Valor en USD: {value}.");
-                        }
+                        ListarMonedas();
                         Thread.Sleep(2000);
                             break;
                     case "2":
-                        // Pedimos valores
-                        Console.Write($" Nombre de la nueva moneda: ");
-                        string nombreNuevaMoneda = Console.ReadLine();
-                        Console.Write($" Codigo de la nueva moneda: ");
-                        string codigoNuevaMoneda = Console.ReadLine();
-                        Console.Write($" Valor de la nueva moneda: ");
-                        float valorEnDolares = float.Parse(Console.ReadLine());
-
-
+                        Console.Clear();
+                        Console.WriteLine($"\n\t CREAR MONEDA (monedas.json)\n");
+                        CrearMonedas();
+                        Thread.Sleep(2000);
                         break;
                     case "3":
                         break;
+                }
+
+                static void ListarMonedas() 
+                {
+                    // Ruta del fichero .json
+                    string rutaJson = "..\\..\\..\\monedas.json";
+
+                    // Guarda el contenido de monedas.json en la variable json
+                    string json = File.ReadAllText(rutaJson);
+
+                    // Deserializa el json en la lista de monedas
+                    List<Monedas> listaMonedas = JsonConvert.DeserializeObject<List<Monedas>>(json);
+
+                    foreach (Monedas moneda in listaMonedas)
+                    {
+                        string name = moneda.nombre;
+                        string code = moneda.codigo;
+                        float value = moneda.valorEnDolares;
+                        Console.WriteLine($"\t Moneda: {name} | Codigo: {code} | Valor en USD: {value}.");
+                    }
+                }
+
+                static void CrearMonedas() 
+                {
+                    // Pedimos valores
+                    Console.Write($" Nombre de la nueva moneda: ");
+                    string nombreNuevaMoneda = Console.ReadLine();
+                    Console.Write($" Codigo de la nueva moneda: ");
+                    string codigoNuevaMoneda = Console.ReadLine();
+                    Console.Write($" Valor de la nueva moneda: ");
+                    float valorEnDolares = float.Parse(Console.ReadLine());
+
+                    // Ruta del fichero .json
+                    string rutaJson = "..\\..\\..\\monedas.json";
+
+                    // Guarda el contenido de monedas.json en la variable json
+                    string json = File.ReadAllText(rutaJson);
                 }
                 
             }
